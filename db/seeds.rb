e406c7bce5a3676216578b308eae117f3a8a5a5e
github username <github email address>
2020-02-28 11:43:58 -0500

    added artwork for lemonade in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 lemonade = Song.create(title:"Lemonade", artist:"Beyonce", duration:180, album:"Lemonade", cover_art:"none")
-halo = Song.create(title:"Halo", artist:"Beyonce", duration:180, album:"I Am Sasha Fierce", cover_art:"none")
+halo = Song.create(title:"Halo", artist:"Beyonce", duration:180, album:"I Am Sasha Fierce", cover_art:"https://images-na.ssl-images-amazon.com/images/I/41c6iGjVtDL._SY400_.jpg")
 rap_god = Song.create(title:"Rap God", artist:"Eminem", duration:180, album:"Marshall Mathers LP", cover_art:"none")
-move = Song.create(title:"Move", artist:"Ludacris", duration:180, album:"Word Of Mouf", cover_art:"none")
+move1 = Song.create(title:"Move", artist:"Ludacris", duration:180, album:"Word Of Mouf", cover_art:"none")
 
 amia = User.create(name:"Amia", username:"amia123")
 skyler = User.create(name:"Skyler", username:"skyler1")
@@ -17,5 +17,5 @@ skyler = User.create(name:"Skyler", username:"skyler1")
 savedsong1 = SavedSong.create(user_id:amia.id, song_id:lemonade.id)
 savedsong2 = SavedSong.create(user_id:skyler.id, song_id:lemonade.id)
 savedsong3 = SavedSong.create(user_id:amia.id, song_id:rap_god.id)
-savedsong4 = SavedSong.create(user_id:skyler.id, song_id:move.id)
+savedsong4 = SavedSong.create(user_id:skyler.id, song_id:move1.id)
 savedsong5 = SavedSong.create(user_id:amia.id, song_id:halo.id)
