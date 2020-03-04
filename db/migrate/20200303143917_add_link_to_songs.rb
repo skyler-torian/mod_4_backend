85f95541c270d15e6ee962291a1b95348830e167
github username <github email address>
2020-03-04 11:48:45 -0500

    removed debugger from render

--- db/migrate/20200303143917_add_link_to_songs.rb
+++
@@ -1,5 +0,0 @@
-class AddLinkToSongs < ActiveRecord::Migration[6.0]
-  def change
-    add_column :songs, :link, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_143917) do
+ActiveRecord::Schema.define(version: 2020_02_28_160019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,9 +28,9 @@ ActiveRecord::Schema.define(version: 2020_03_03_143917) do
     t.integer "duration"
     t.string "album"
     t.string "cover_art"
+    t.string "link"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "link"
   end
 
   create_table "users", force: :cascade do |t|
